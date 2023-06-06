7f32947f3edea2869fb2a3b8d99dfbdd91f51f6d
AndreaBar94 <andreabarocchi@gmail.com>
2023-06-06 17:21:32 +0200

    endpoint param

--- GestionePrenotazioni/src/main/java/andrea/GestionePrenotazioni/controllers/InfoController.java
+++ GestionePrenotazioni/src/main/java/andrea/GestionePrenotazioni/controllers/InfoController.java
@@ -2,24 +2,19 @@ package andrea.GestionePrenotazioni.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/info")
 public class InfoController {
-	
-	@GetMapping("")
-	public String home() {
-		return "Inserisci /it per le info in italiano || Insert /en for english language info";
-	}
-	
-    @GetMapping("/it")
-    public String homeIt() {
-        return "Benvenuto, a seguire le regole per la prenotazione: ...";
-    };
 
-    @GetMapping("/en")
-    public String homeEn() {
-        return "Welcome, here you are our reservation rules: ...";
-    };
+    @GetMapping("")
+    public String home(@RequestParam(defaultValue = "it") String lang) {
+        if (lang.equals("it")) {
+            return "Benvenuto, a seguire le regole per la prenotazione: ...";
+        } else {
+            return "Welcome, here you are our reservation rules: ...";
+        }
+    }
 }
